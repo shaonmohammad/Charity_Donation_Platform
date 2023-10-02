472ba30e1add7246af4c27f649ba7d7a216694a5
Md Golam Moctader <golam.moctader@gmail.com>
2023-10-02 11:36:45 +0300

    payment method

--- user_app/models.py
+++ user_app/models.py
@@ -7,6 +7,17 @@ class UserInformation(models.Model):
     email = models.EmailField(max_length=100)
     mobile = models.CharField(max_length=100)
     role = models.CharField(max_length=10)
+    amount=models.IntegerField()
 
     def __str__(self) -> str:
         return self.username
+    
+class PaymentGatewaySettings(models.Model):
+    
+    store_id = models.CharField(max_length=500, blank=True, null=True)
+    store_pass = models.CharField(max_length=500, blank=True, null=True)
+    
+    class Meta:
+        verbose_name = "PaymentGatewaySetting"
+        verbose_name_plural = "PaymentGatewaySettings"
+        db_table = "paymentgatewaysettings"
--- user_app/views.py
+++ user_app/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render,redirect
 from .models import UserInformation
+from .ssl import sslcommerz_payment_gateway
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from .models import PaymentGatewaySettings
+from django.views import View
+
 
 # Create your views here.
+@method_decorator(csrf_exempt, name='dispatch')
+def CheckoutSuccessView(request):
+    return render(request, 'success.html')
 
 def register(request):
     user_registration = None
@@ -10,8 +19,9 @@ def register(request):
         email = request.POST['email']
         mobile = request.POST['mobile']
         role = request.POST['role']
-        user_registration = UserInformation(username=username,email = email,mobile = mobile, role = role)
+        amount = request.POST['amount']
+        user_registration = UserInformation(username=username,email = email,mobile = mobile, role = role, amount=amount)
         user_registration.save()
-        return redirect('register')
+        return redirect(sslcommerz_payment_gateway(request, amount))
         #print(username, email, mobile, role)
     return render(request, 'register.html')
