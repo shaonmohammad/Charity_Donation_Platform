26772bdbf010421e1c03437c7d8ce2d810ee8815
Tamjid7401 <mdadnan1999@gmail.com>
2023-10-04 22:37:59 -0500

    Added Tinymce

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-
+from tinymce.models import HTMLField
 # Create your models here.
 class BlogModel(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=50)
-    body = models.TextField()
+    body = HTMLField()
     
     def __str__(self):
         return self.title + ' | ' + str(self.author)
