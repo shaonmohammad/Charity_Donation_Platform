33b900c241e66b1b78c25f140f22a9ae51daf2a3
Shoan <shaonmohammad13@gmail.com>
2023-10-13 10:32:28 +0600

    task completed

--- user_app/models.py
+++ user_app/models.py
@@ -8,21 +8,14 @@ class UserInformation(models.Model):
     email = models.EmailField(max_length=100)
     mobile = models.CharField(max_length=100)
     role = models.CharField(max_length=10)
-<<<<<<< HEAD
     amount = models.DecimalField(
-        max_digits=10, decimal_places=2, default=0.0, null=True, blank=True)
-=======
-    # amount=models.IntegerField(null=True)
-    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.0, null=True)
-
->>>>>>> 220a10c14e66a5060a6c799fe758bdc77d3d9321
+        max_digits=10, decimal_places=2, null=True, blank=True)
 
     def __str__(self) -> str:
         return self.username
 
 
 class PaymentGatewaySettings(models.Model):
-
     store_id = models.CharField(max_length=500, blank=True, null=True)
     store_pass = models.CharField(max_length=500, blank=True, null=True)
 
